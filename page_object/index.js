a17477f632f28b100e2d4941c6a78070d0ad4f5c
Yury Huminski <113417816+YuryHuminski@users.noreply.github.com>
2026-04-21 19:51:33 +0200

    Register QavaDocsIntroPage in page object app registry

--- page_object/index.js
+++ page_object/index.js
@@ -1,7 +1,8 @@
 const { locator } = require("@qavajs/steps-playwright/po.js");
 const QavaHomePage = require("./QavaHomePage");
+const QavaDocsIntroPage = require("./QavaDocsIntroPage");
 
 module.exports = class App {
   QavaHomePage = locator.as(QavaHomePage);
-
+  QavaDocsIntroPage = locator.as(QavaDocsIntroPage);
 }
