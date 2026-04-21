af99118d5383a5173657d008a0e749c70c276128
Yury Huminski <113417816+YuryHuminski@users.noreply.github.com>
2026-04-21 19:54:51 +0200

    Improve Steps dropdown locators scoping to Steps navbar dropdown

--- page_object/QavaDocsIntroPage.js
+++ page_object/QavaDocsIntroPage.js
@@ -10,13 +10,15 @@ module.exports = class QavaDocsIntroPage {
     WhatsNewInV2Link = locator(`nav.menu a.menu__link:has-text("What's new in v2")`);
 
     /** Top navigation "Steps" dropdown */
-    StepsDropdownToggle = locator('nav.navbar a:has-text("Steps")');
-    StepsDropdownMenu = locator('nav.navbar .dropdown__menu');
-    StepsDropdownLinks = locator('nav.navbar .dropdown__menu .dropdown__link');
+    StepsDropdown = locator('nav.navbar .navbar__item.dropdown:has(a:has-text("Steps"))');
+    StepsDropdownToggle = locator('nav.navbar .navbar__item.dropdown:has(a:has-text("Steps")) > a');
+    StepsDropdownMenu = locator('nav.navbar .navbar__item.dropdown:has(a:has-text("Steps")) .dropdown__menu');
+    StepsDropdownLinks = locator('nav.navbar .navbar__item.dropdown:has(a:has-text("Steps")) .dropdown__menu .dropdown__link');
 
     /**
      * Helper for Steps dropdown packages.
      * @param {string} packageName
      */
-    stepsPackageLink = (packageName) => locator(`nav.navbar .dropdown__menu .dropdown__link:has-text("${packageName}")`);
+    stepsPackageLink = (packageName) =>
+        locator(`nav.navbar .navbar__item.dropdown:has(a:has-text("Steps")) .dropdown__menu .dropdown__link:has-text("${packageName}")`);
 }
